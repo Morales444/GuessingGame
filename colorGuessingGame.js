d0892f84b37c970ea981f8a90c0b9e5b911baf9a
Morales444 <gisselle.ortega444@524-wdmlg8wn.local>
2016-11-09 13:41:44 -0800

    added try each and some CSS

--- colorGuessingGame.js
+++ colorGuessingGame.js
@@ -15,36 +15,35 @@ function game(){
 
 
     //indexOF
-    do {
+    try {
+        do {
 
-        userChoice = prompt("Maroon, Aqua, Green, Silver, Black, White, Purple, Pink, Azure, Gold\n" +
-            "Which color am i thinking of", "");
-        if ( colors.indexOf(userChoice.toUpperCase()) == -1  ){
-            alert("Sorry, this is not a valid color or an empty string");
+            userChoice = prompt("Maroon, Aqua, Green, Silver, Black, White, Purple, Pink, Azure, Gold\n" +
+                "Which color am i thinking of?", "");
+            if ( colors.indexOf(userChoice.toUpperCase()) == -1  ){
+                alert("Sorry, this is not a valid color or an empty string");
+            }
+            else if (colors.indexOf(userChoice.toUpperCase())> random) {
+                alert ("Sorry, this color is incorrect \n" +
+                    "Hint: your color is alphabetically higher than the answer \n" +
+                    "Please try again"
+                )
+            }
+            else if (colors.indexOf(userChoice.toUpperCase())< random) {
+                alert ("Sorry, this color is incorrect \n" +
+                    "Hint: your color is alphabetically lower than the answer \n" +
+                    "Please try again")
+            }
+            else {
+                alert("Congratulations!!! You guessed the right color" );
+                var myBody = document.getElementsByTagName("body")[0];
+                myBody.style.background= finCol;
+                finished = true;
+            }
         }
-        else if (colors.indexOf(userChoice.toUpperCase())> random) {
-            alert ("Sorry, this color is incorrect \n" +
-                "Hint: your color is alphabetically higher than the answer \n" +
-                "Please try again"
-            )
-        }
-        else if (colors.indexOf(userChoice.toUpperCase())< random) {
-            alert ("Sorry, this color is incorrect \n" +
-                "Hint: your color is alphabetically lower than the answer \n" +
-                "Please try again")
-        }
-        else {
-            alert("Congratulations!!! You guessed the right color" );
-            var myBody = document.getElementsByTagName("body")[0];
-            myBody.style.background= finCol;
-            finished = true;
-        }
-    }
-    while (
+        while (
         finished == false)
-
-
-
-
-
+    } catch (e) {
+        console.log(e)
+    }
 }
